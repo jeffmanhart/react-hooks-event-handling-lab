e00a80ac0184bf43befb8865be26b7b85abe884a
jeffmanhart <jeffreymanhart@gmail.com>
2023-10-30 13:36:24 -0600

    made all them tests pass!

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,11 @@
+import React from 'react';
 // Code Keypad Component Here
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input onChange={()=>console.log("Entering password...")} type= {"password"}></input>
+        </div>
     )
 }
 
